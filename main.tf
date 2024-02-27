5221dfcf2f2f2cbea0a07fca32a41c42c73f2bf1
pamano21 <145840762+pamano21@users.noreply.github.com>
2024-02-28 00:09:53 +0100

    fin
    ddd

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "google_cloud_run_service" "edemdevsecops1" {
-  name     = "edemdevsecops1"
+resource "google_cloud_run_service" "edemdevsecops" {
+  name     = "edemdevsecops"
   location = var.region
 
   template {
@@ -28,7 +28,7 @@ data "google_iam_policy" "noauth" {
 resource "google_cloud_run_service_iam_policy" "noauth" {
 
   depends_on = [
-    google_cloud_run_service.edemdevsecops1,
+    google_cloud_run_service.edemdevsecops,
   ]
 
   location    = var.region
