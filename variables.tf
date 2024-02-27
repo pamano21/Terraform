b5b26f41fa2e716bbb9aa114bfe273f569cc559e
pamano21 <145840762+pamano21@users.noreply.github.com>
2024-02-27 23:21:56 +0100

    cambios
    nnnn

--- variables.tf
+++ variables.tf
@@ -1,10 +1,19 @@
-variable "project_id" { type = string
+variable "project_id" {
+    type  = string
 }
-variable "region" { type = string
+
+variable "region" {
+    type  = string
 }
-variable "zone" { type = string
+
+variable "zone" {
+    type  = string
 }
-variable "service" { type = string
+
+variable "service" {
+    type  = string
 }
-variable "image_tag" { type = string
+
+variable "image_tag" {
+    type  = string
 }
